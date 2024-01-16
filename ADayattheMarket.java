3864e0d45bfa96c89981da97670896237456d127
Samet Can <74251810+sametcanugur@users.noreply.github.com>
2024-01-16 21:36:20 +0300

    Add files via upload

---
+++ ADayattheMarket.java
@@ -0,0 +1,18 @@
+package Sorular;
+
+public class ADayattheMarket {
+
+	public static void main(String[] args) {
+		System.out.println(aDayAtTheMarket(3, 7, 2, 5));
+	}
+
+	public static String aDayAtTheMarket(int billMoney, int willSpace, int price, int size) {
+		if ((billMoney / price) > (willSpace / size)) {
+			return "Bill";
+		} else if ((billMoney / price) < (willSpace / size)) {
+			return "Will";
+		} else {
+			return "Tie";
+		}
+	}
+}
